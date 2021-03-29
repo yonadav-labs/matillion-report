dbe3e2d39ee35d49654724dff6826ee215476826
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2021-03-29 01:51:30 -0700

    complete opp tab with opp pipeline

--- main.py
+++ main.py
@@ -32,8 +32,7 @@ def process_lead_tab():
     df.fillna(country_value, inplace=True)
 
     # add seller name
-    df = df.reindex(columns=['Seller Company Name']+list(df.columns))
-    df['Seller Company Name'] = 'Matillion'
+    df.insert(loc=0, column='Seller Company Name', value='Matillion')
 
     # remove duplicates
     df.drop_duplicates(subset=['Campaign ID'], inplace=True)
@@ -60,9 +59,12 @@ def process_lead_tab():
 def process_opp_tab_1():
     df_opp_revenue = pd.read_csv('data/opp-revenue.csv')
     df_opp_revenue = remove_footer(df_opp_revenue)
+    df_opp_pipeline = pd.read_csv('data/opp-pipeline.csv')
+    df_opp_pipeline = remove_footer(df_opp_pipeline)
     df_contacts = pd.read_csv('data/contacts.csv')
     df_contacts = remove_footer(df_contacts)
 
+    # filter by marketing touches
     df_opp_revenue.drop(['BI Cloud Data Warehouse', 'Amount (ACV) Currency'], axis=1, inplace=True)
     df_mark_down = pd.pivot_table(df_contacts, values=['Contact 18 Character ID'], index=['Record Unique ID'], aggfunc=np.count_nonzero)
 
@@ -70,19 +72,21 @@ def process_opp_tab_1():
     df_opp_revenue = df_opp_revenue[df_opp_revenue['Contact 18 Character ID'] > 4]
 
     df_opp_revenue['Stage'] = 'Closed'
+    df_opp_pipeline['Stage'] = 'Valid'
+
+    # remove columns
+    df_opp_revenue.drop(['Contact 18 Character ID'], axis=1, inplace=True)
+    df_opp_pipeline.drop(['Amount (ACV) Currency'], axis=1, inplace=True)
+
+    df_opp = pd.concat([df_opp_revenue, df_opp_pipeline])
     marketplace_opp_values = {
         0: 'Not reportable',
         1: 'Yes'
     }
-    df_opp_revenue['AWS Marketplace Opportunity?'].replace(marketplace_opp_values, inplace=True)
-
-    # remove columns
-    df_opp_revenue.drop(['Contact 18 Character ID', 'Amount (ACV)'], axis=1, inplace=True)
-
-    df_opp_revenue.insert(loc=1, column='Seller Company Name', value='Matillion')
+    df_opp['AWS Marketplace Opportunity?'].replace(marketplace_opp_values, inplace=True)
+    df_opp.insert(loc=1, column='Seller Company Name', value='Matillion')
 
     df_contacts.drop(['Member First Associated Date', 'Contact 18 Character ID'], axis=1, inplace=True)
-
     df_contacts = df_contacts[df_contacts['Campaign Source'] != 'Other']
     df_contacts.sort_values(by=['Created Date'], inplace=True)
     df_contacts.drop_duplicates(subset=['Record Unique ID'], inplace=True)
@@ -93,28 +97,35 @@ def process_opp_tab_1():
     }
     df_contacts.rename(new_column_names, axis=1, inplace=True)
 
-    df_opp_revenue = df_opp_revenue.merge(df_contacts, on='Record Unique ID', how='left')
-    df_opp_revenue.dropna(inplace=True)
+    df_opp = df_opp.merge(df_contacts, on='Record Unique ID', how='left')
+    df_opp.dropna(subset=['GTM Campaign Source'], inplace=True)
 
     # change column name
-    df_opp_revenue.rename({'First Usage Date': 'Win Date'}, axis=1, inplace=True)
-    df_opp_revenue.to_excel('data/opp-4.xlsx', sheet_name='Lead Level', index=False)
+    new_column_names = {
+        'First Usage Date': 'Win Date',
+        'Opportunity 18 Character ID': 'Opportunity ID',
+        'Created Date': 'Convert Date',
+        'Billing Country': 'Opportunity Country',
+        'Stage': 'Opportunity Status',
+        'Amount (ACV)': 'Pipeline Revenue'
+    }
+    df_opp.rename(new_column_names, axis=1, inplace=True)
+    df_opp.drop_duplicates(subset=['Opportunity ID'], inplace=True)
 
+    # reorder columns
+    columns = list(df_opp.columns)
+    new_columns = columns[:2] + columns[12:] + columns[2:12]
+    df_opp = df_opp[new_columns]
+    df_opp.insert(loc=14, column='CPPO', value='')
+    df_opp.insert(loc=15, column='Consulting Partner Name', value='')
 
-def process_opp_tab_3():
-    df = pd.read_csv('data/opp-pipeline.csv')
-    df = remove_footer(df)
-
-    # remove columns
-    df.drop(['BI Cloud Data Warehouse', 'Amount (ACV) Currency'], axis=1, inplace=True)
-    df.to_excel('data/opp-2.xlsx', sheet_name='Lead Level', index=False)
+    df_opp.to_excel('data/opp-1.xlsx', sheet_name='Opportunity Level', index=False)
 
 
 def main():
     # download_reports()
     # process_lead_tab()
     process_opp_tab_1()
-    # process_opp_tab_2()
     # process_opp_tab_3()
 
 
